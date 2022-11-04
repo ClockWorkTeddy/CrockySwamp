edd252e310c6f3b44943363a9799a4dbe623fee0
ivan zhukov <vankaent@live.com>
2022-11-04 15:08:23 +0300

    Created class for "Field"

---
+++ CrockySwamp/Field.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrockySwamp
+{
+    internal class Field
+    {
+        public enum FieldState
+        {
+            Crock,
+            Frog,
+            Empty
+        }
+
+        public Point Location { get; set; }
+        public FieldState State { get; set; }
+
+        public Field (Point location, FieldState state = FieldState.Empty)
+        {
+            Location = location;
+            State = state;
+        }
+    }
+}
---
+++ CrockySwamp/Swamp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrockySwamp
+{
+    internal class Swamp
+    {
+        public int Size { get; set; }
+
+    }
+}
