297939d4b89916c63e6dbface4b7a96be7cc1748
ivan zhukov <vankaent@live.com>
2022-11-04 15:30:00 +0300

    Created constructor, that creates Swamp With Fields (size*size)

--- CrockySwamp/Swamp.cs
+++ CrockySwamp/Swamp.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing;
 namespace CrockySwamp
 {
     internal class Swamp
     {
         public int Size { get; set; }
+        public List<Field> Fields { get; set; } = new List<Field>();
+        public int CrocksCount { get; set; }
+        public int FrogsCount { get; set; }
 
+        public Swamp(int size)
+        {
+            Size = size;
+            for (int i = 0; i < Size; i++)
+            {
+                int x = i / Size;
+                int y = i % Size;
+                Fields.Add(new Field(new Point(x, y)));
+            }
+        }
     }
 }
