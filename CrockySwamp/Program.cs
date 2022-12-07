17fed12abf52d6ed4ff2159ec83a774596e8ee1f
ivan zhukov <vankaent@live.com>
2022-12-07 21:57:52 +0300

    Refactoring

--- CrockySwamp/Program.cs
+++ CrockySwamp/Program.cs
@@ -3,92 +3,111 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-List<string> messages = new List<string>();
-
-var server = new TcpListener(IPAddress.Parse("192.168.31.69"), 8888);
-server.Start();
-
-var setUpData = await GetStartUpData(server);
-
-int size = Convert.ToInt16(setUpData.Split(new char[] { ' ' })[0]);
-string name = setUpData.Split(new char[] { ' ' })[1];
-
-Swamp swamp = new(size);
-Naturalist naturalist = new(swamp, name);
-
-while ((await XTalk(server) != "q"));
-    NextStep(swamp, naturalist);
-
-
-server.Stop();
-
-
-async Task<string> GetStartUpData(TcpListener server)
+namespace CrockySwamp
 {
-    using var tcpClient = await server.AcceptTcpClientAsync();
-    var stream = tcpClient.GetStream();
+    public static class Program
+    {
+        static TcpListener Server;
+        static TcpClient Client;
+        async public static Task Main(string[] args)
+        {
+            List<string> messages = new List<string>();
 
-    StartUp(stream);
+            Server = new TcpListener(IPAddress.Parse("192.168.31.243"), 8888);
+            Server.Start();
+            Client = await Server.AcceptTcpClientAsync();
 
-    var result = await ReceiveClientData(stream);
+            List<string> greetings = SetGreetings();
 
-    return result;
-}
+            var setUpData = await XTalk(greetings);
 
-async Task<string> XTalk(TcpListener server)
-{
-    using var tcpClient = await server.AcceptTcpClientAsync();
-    var stream = tcpClient.GetStream();
-    var result = await ReceiveClientData(stream);
+            int size = Convert.ToInt16(setUpData.Split(new char[] { ' ' })[0]);
+            string name = setUpData.Split(new char[] { ' ' })[1];
 
-    return result;
-}
+            Swamp swamp = new(size);
+            Naturalist naturalist = new(swamp, name);
 
-async void StartUp(NetworkStream stream)
-{
-    List<string> greetings = SetGreetings();
-    var responce = GetResponce(greetings);
-    await stream.WriteAsync(responce);
-}
+            while (await XTalk(new List<string> { "press" }) != "q")
+                NextStep(swamp, naturalist);
 
-async Task<string> ReceiveClientData(NetworkStream stream)
-{
-    int bytesRead = 10;
-    List<byte> query = new List<byte>();
 
-    while ((bytesRead = stream.ReadByte()) != '#')
-        query.Add((byte)bytesRead);
+            Server.Stop();
+        }
 
-    var data = Encoding.UTF8.GetString(query.ToArray());
+        async static Task<string> GetStartUpData()
+        {
+            var stream = Client.GetStream();
 
-    return data;
-}
+            StartUp(stream);
 
-byte[] GetResponce(List<string> data)
-{
-    StringBuilder responce = new StringBuilder();
+            var result = await ReceiveClientData(stream);
 
-    foreach (var message in data)
-        responce.Append(message);
+            return result;
+        }
 
-    return Encoding.UTF8.GetBytes(responce.ToString());
-}
+        async static Task<string> XTalk(List<string> messages)
+        {
+            var stream = Client.GetStream();
 
-List<string> SetGreetings()
-{
-    List<string> result = new List<string>();
+            if (messages.Count > 0)
+            {
+                var responce = GetResponce(messages);
+                await stream.WriteAsync(responce);
+            }
 
-    result.Add("Hello! There will be a swamp with corcks and frogs! And naturalist!@");
-    result.Add("Please, input swamp size and naturalist's name devided by space.@" );
-    result.Add("#@");
+            var result = await ReceiveClientData(stream);
 
-    return result;
-}
+            return result;
+        }
 
-void NextStep(Swamp swamp, Naturalist naturalist)
-{
-    Console.Clear();
-    swamp.Move();
-    naturalist.Observe();
+        async static void StartUp(NetworkStream stream)
+        {
+            List<string> greetings = SetGreetings();
+
+            var responce = GetResponce(greetings);
+            await stream.WriteAsync(responce);
+        }
+
+        async static Task<string> ReceiveClientData(NetworkStream stream)
+        {
+            int bytesRead = 10;
+            List<byte> query = new List<byte>();
+
+            while ((bytesRead = stream.ReadByte()) != '#')
+                query.Add((byte)bytesRead);
+
+            var data = Encoding.UTF8.GetString(query.ToArray());
+
+            return data;
+        }
+
+        static byte[] GetResponce(List<string> data)
+        {
+            StringBuilder responce = new StringBuilder();
+
+            foreach (var message in data)
+                responce.Append(message);
+
+            return Encoding.UTF8.GetBytes(responce.ToString());
+        }
+
+        static List<string> SetGreetings()
+        {
+            List<string> result = new List<string>();
+
+            result.Add("Hello! There will be a swamp with corcks and frogs! And naturalist!@");
+            result.Add("Please, input swamp size and naturalist's name devided by space.@");
+            result.Add("#@");
+
+            return result;
+        }
+
+        static void NextStep(Swamp swamp, Naturalist naturalist)
+        {
+            Console.Clear();
+            swamp.Move();
+            naturalist.Observe();
+        }
+    }
 }
 
